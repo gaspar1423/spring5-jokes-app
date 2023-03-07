aa1ebafdf6e953e15825a0bc4b26966ef2d71c5b
Gaspar Oliveira <goliveira@noxhealth.com>
2023-03-07 00:42:26 +0000

    Add Controller and View

--- src/main/java/goliveira/udemy/jokes/services/JokesServiceImpl.java
+++ src/main/java/goliveira/udemy/jokes/services/JokesServiceImpl.java
@@ -1,7 +1,9 @@
 package goliveira.udemy.jokes.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.stereotype.Service;
 
+@Service
 public class JokesServiceImpl implements JokesService {
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
